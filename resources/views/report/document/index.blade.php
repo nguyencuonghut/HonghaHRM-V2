c4f9dd4e72152d9c523d068d91cb6044651730b6
Tony Ng <nguyencuonghut55@gmail.com>
2024-11-29 10:37:31 +0700

    Fix: correct the title of Document report

--- resources/views/report/document/index.blade.php
+++ resources/views/report/document/index.blade.php
@@ -11,7 +11,7 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0">Theo dõi hồ sơ LĐ</h1>
+          <h1 class="m-0">Báo cáo hồ sơ LĐ</h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
