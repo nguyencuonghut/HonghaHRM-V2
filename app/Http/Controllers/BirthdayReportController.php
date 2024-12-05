afe4826f14a7e08b197c7f7b49bde9fab9128c37
Tony Ng <nguyencuonghut55@gmail.com>
2024-12-05 13:45:40 +0700

    Fix: correct the bug of position column on Birthday report

--- app/Http/Controllers/BirthdayReportController.php
+++ app/Http/Controllers/BirthdayReportController.php
@@ -26,28 +26,31 @@ class BirthdayReportController extends Controller
                     return '<a href=' . route("employees.show", $data->id) . '>' . $data->name . '</a>' ;
                 })
                 ->editColumn('position', function ($data) {
-                    $my_position_ids = Work::where('employee_id', $data->id)
-                                    ->where(function ($query) {
-                                        $query->whereIn('off_type_id', [2,3,4,5])//2: Nghỉ thai sản, 3: Nghỉ không lương, 4: Nghỉ ốm, 5: Thay đổi chức danh
-                                            ->orWhereNull('off_type_id');
-                                    })
-                                    ->pluck('position_id')
-                                    ->toArray();
-                    $my_positions = Position::whereIn('id', $my_position_ids)->get();
+                    $position_arr = [];
                     $position_str = '';
-                    $i = 0;
-                    $length = count($my_positions);
-                    if ($length) {
-                        foreach ($my_positions as $my_position) {
-                            if(++$i === $length) {
-                                $position_str .= $my_position->name;
-                            } else {
-                                $position_str .= $my_position->name;
-                                $position_str .= ' | ';
+                    //Tìm tất cả Works
+                    $works = Work::where('employee_id', $data->id)->get();
+                    if (0 == $works->count()) {
+                        return 'Chưa có QT công tác';
+                    } else {//Đã có QT công tác
+                        $on_works = Work::where('employee_id', $data->id)
+                                        ->where('status', 'On')
+                                        ->get();
+                        if ($on_works->count()) {//Có QT công tác ở trạng thái On
+                            foreach ($on_works as $on_work) {
+                                array_push($position_arr, $on_work->position->name);
                             }
+                        } else {//Còn lại là các QT công tác ở trạng thái Off
+                            $last_off_works = Work::where('employee_id', $data->id)
+                                            ->where('status', 'Off')
+                                            ->orderBy('start_date', 'desc')
+                                            ->first();
+                            array_push($position_arr, $last_off_works->position->name);
                         }
-                    } else {
-                        $position_str .= '!! Chưa gán vị trí công việc !!';
+                        //Xóa các position trùng nhau
+                        $position_arr = array_unique($position_arr);
+                        //Convert array sang string
+                        $position_str = implode(' | ', $position_arr);
                     }
                     return $position_str;
                 })
